30ce3cb382699d907418d24d2f34d965f859ec32
henryppi <green.spam@web.de>
2026-02-20 16:08:42 +0100

    update interactive joukowsky

--- run_joukowsky_transform_interactive.py
+++ run_joukowsky_transform_interactive.py
@@ -55,38 +55,43 @@ class JoukowskyTransform:
         points[:,1] = z_trans.imag
         return points
         
+def distance(points,x,y):
+    return np.sqrt((points[:,0]-x)**2+(points[:,1]-y)**2)
 
 class CircleInteractor:
     showverts = True
     epsilon = 5  # max pixel distance to count as a vertex hit
 
-    def __init__(self,ax_circ):
-        self.ax = ax_circ
+    
+    def __init__(self,ax):
+        self.ax = ax
         canvas = self.ax.figure.canvas
-        # self.pathpatch = pathpatch
-        # self.pathpatch.set_animated(True)
 
-        cx = -0.12
-        cy = 0.08
+        self.cx = -0.12
+        self.cy = 0.08
         n = 100
-        rad = 1.15
-        ang = 20.0*np.pi/180
+        self.rad = 1.15
+        self.ang = 10.0*np.pi/180
         
-        p0 = np.array([cx,cy])
-        p1 = np.array([cx+rad*np.cos(ang),cy+rad*np.sin(ang)])
+        self.points = np.array([[self.cx,self.cy],\
+                          [self.cx+self.rad*np.cos(self.ang),self.cy+self.rad*np.sin(self.ang)]])
+
+        self._ind = None  # the active vertex
+
+        x= self.points[:,0]
+        y = self.points[:,1]
+        self.line, = self.ax.plot(x, y, marker='o', markerfacecolor='r', animated=True)
 
         self.JT = JoukowskyTransform()
-        unit = J.get_unit_circle()
-        self.J.set_para(cx,cy,rad,ang)
-        self.J.redraw()
-        circ = J.get_circle()
-        wing = J.get_transformed()
+        unit = self.JT.get_unit_circle()
+        self.JT.set_para(self.cx,self.cy,self.rad,self.ang)
+        self.JT.redraw()
+        circ = self.JT.get_circle()
+        wing = self.JT.get_transformed()
 
-        self.unit = ax.plot(unit[:,0],unit[:,1],)
-
-        self.ax_unit, = ax.plot(unit[:,0],unit[:,1],'--k',lw=0.5,animated=True)
-        self.ax_circ, = ax.plot(circ[:,0],circ[:,1],'-k',lw=1.5,animated=True)
-        self.ax_wing, = ax.plot(wing[:,0],wing[:,1],'-b',lw=3,animated=True)
+        self.unit, = self.ax.plot(unit[:,0],unit[:,1],'--k',lw=0.5,animated=True)
+        self.circ, = self.ax.plot(circ[:,0],circ[:,1],'-k',lw=1.5,animated=True)
+        self.wing, = self.ax.plot(wing[:,0],wing[:,1],'-b',lw=3,animated=True)
         
         self.ax.axis('equal')
 
@@ -102,29 +107,37 @@ class CircleInteractor:
         Return the index of the point closest to the event position or *None*
         if no point is within ``self.epsilon`` to the event position.
         """
-        xy = self.pathpatch.get_path().vertices
-        xyt = self.pathpatch.get_transform().transform(xy)  # to display coords
-        xt, yt = xyt[:, 0], xyt[:, 1]
-        d = np.sqrt((xt - event.x)**2 + (yt - event.y)**2)
+        # print('get_ind_under_point')
+        pixel_coords = (event.x,event.y)
+        inv_trans = self.ax.transData.inverted()
+        data_coords = inv_trans.transform(pixel_coords)
+        d = distance(self.points,data_coords[0],data_coords[1])        
+
         ind = d.argmin()
         return ind if d[ind] < self.epsilon else None
 
     def on_draw(self, event):
         """Callback for draws."""
+        # print('on_draw')
         self.background = self.canvas.copy_from_bbox(self.ax.bbox)
-        self.ax.draw_artist(self.pathpatch)
+        self.ax.draw_artist(self.unit)
+        self.ax.draw_artist(self.circ)
+        self.ax.draw_artist(self.wing)
         self.ax.draw_artist(self.line)
 
     def on_button_press(self, event):
         """Callback for mouse button presses."""
+        # print('on_button_press')
         if (event.inaxes is None
                 or event.button != MouseButton.LEFT
                 or not self.showverts):
             return
         self._ind = self.get_ind_under_point(event)
+        
 
     def on_button_release(self, event):
         """Callback for mouse button releases."""
+        # print('on_button_release')
         if (event.button != MouseButton.LEFT
                 or not self.showverts):
             return
@@ -132,6 +145,7 @@ class CircleInteractor:
 
     def on_key_press(self, event):
         """Callback for key presses."""
+        # print('on_key_press')
         if not event.inaxes:
             return
         if event.key == 't':
@@ -143,165 +157,52 @@ class CircleInteractor:
 
     def on_mouse_move(self, event):
         """Callback for mouse movements."""
+        # print('on_mouse_move')
         if (self._ind is None
                 or event.inaxes is None
                 or event.button != MouseButton.LEFT
                 or not self.showverts):
             return
 
-        vertices = self.pathpatch.get_path().vertices
+        if self._ind == 0:
+            #center move
+            self.cx = event.xdata
+            self.cy = event.ydata
+            
+        elif self._ind == 1:
+            # rad / ang move
+            self.rad = np.sqrt((self.cx-event.xdata)**2+(self.cy-event.ydata)**2)
+            self.ang = np.arctan2(-(self.cy-event.ydata),-(self.cx-event.xdata))
+        else:
+            pass
 
-        vertices[self._ind] = event.xdata, event.ydata
-        self.line.set_data(zip(*vertices))
+        self.points[0,:] = [self.cx, self.cy]
+        self.points[1,:] = [self.cx+self.rad*np.cos(self.ang), self.cy+self.rad*np.sin(self.ang)]
+        
+        self.JT.set_para(self.cx,self.cy,self.rad,self.ang)
+        self.JT.redraw()
+        circ = self.JT.get_circle()
+        wing = self.JT.get_transformed()
+
+        self.circ.set_data(circ[:,0],circ[:,1])
+        self.wing.set_data(wing[:,0],wing[:,1])
+        self.line.set_data(self.points[:,0],self.points[:,1])
 
         self.canvas.restore_region(self.background)
-        self.ax.draw_artist(self.pathpatch)
+        self.ax.draw_artist(self.unit)
+        self.ax.draw_artist(self.circ)
+        self.ax.draw_artist(self.wing)
         self.ax.draw_artist(self.line)
         self.canvas.blit(self.ax.bbox)
 
+fig, ax = plt.subplots(figsize=(8,8),facecolor='w',frameon=False)
+fig.patch.set_facecolor('none') 
+ax.patch.set_facecolor('none')
 
-
-fig, ax = plt.subplots()
-
-cx = -0.12
-cy = 0.08
-n = 100
-rad = 1.15
-ang = 20.0*np.pi/180
-
-J = JoukowskyTransform()
-unit = J.get_unit_circle()
-J.set_para(cx,cy,rad,ang)
-J.redraw()
-circ = J.get_circle()
-wing = J.get_transformed()
-
-ax_unit = ax.plot(unit[:,0],unit[:,1],'--k',lw=0.5)
-ax_circ = ax.plot(circ[:,0],circ[:,1],'-k',lw=1.5)
-ax_wing = ax.plot(wing[:,0],wing[:,1],'-b',lw=3)
-ax.axis('equal')
+interactor = CircleInteractor(ax)
+ax.set_title('Joukowsky Transformation (drag points to deform)')
+ax.set_xlim(-2.5, 2.5)
+ax.set_ylim(-2.5, 2.5)
+ax.axis('off')
+plt.savefig('./images/joukowsky_transformation_interactive.png',bbox_inches='tight',dpi=200)
 plt.show()
-
-
-
-
-# pathdata = [
-#     (Path.MOVETO, (1.58, -2.57)),
-#     (Path.CURVE4, (0.35, -1.1)),
-#     (Path.CURVE4, (-1.75, 2.0)),
-#     (Path.CURVE4, (0.375, 2.0)),
-#     (Path.LINETO, (0.85, 1.15)),
-#     (Path.CURVE4, (2.2, 3.2)),
-#     (Path.CURVE4, (3, 0.05)),
-#     (Path.CURVE4, (2.0, -0.5)),
-#     (Path.CLOSEPOLY, (1.58, -2.57)),
-# ]
-
-# codes, verts = zip(*pathdata)
-# path = Path(verts, codes)
-# patch = PathPatch(
-#     path, facecolor='green', edgecolor='yellow', alpha=0.5)
-# ax.add_patch(patch)
-
-
-# class PathInteractor:
-#     """
-#     A path editor.
-
-#     Press 't' to toggle vertex markers on and off.  When vertex markers are on,
-#     they can be dragged with the mouse.
-#     """
-
-#     showverts = True
-#     epsilon = 5  # max pixel distance to count as a vertex hit
-
-#     def __init__(self, pathpatch):
-
-#         self.ax = pathpatch.axes
-#         canvas = self.ax.figure.canvas
-#         self.pathpatch = pathpatch
-#         self.pathpatch.set_animated(True)
-
-#         x, y = zip(*self.pathpatch.get_path().vertices)
-
-#         self.line, = ax.plot(
-#             x, y, marker='o', markerfacecolor='r', animated=True)
-
-#         self._ind = None  # the active vertex
-
-#         canvas.mpl_connect('draw_event', self.on_draw)
-#         canvas.mpl_connect('button_press_event', self.on_button_press)
-#         canvas.mpl_connect('key_press_event', self.on_key_press)
-#         canvas.mpl_connect('button_release_event', self.on_button_release)
-#         canvas.mpl_connect('motion_notify_event', self.on_mouse_move)
-#         self.canvas = canvas
-
-#     def get_ind_under_point(self, event):
-#         """
-#         Return the index of the point closest to the event position or *None*
-#         if no point is within ``self.epsilon`` to the event position.
-#         """
-#         xy = self.pathpatch.get_path().vertices
-#         xyt = self.pathpatch.get_transform().transform(xy)  # to display coords
-#         xt, yt = xyt[:, 0], xyt[:, 1]
-#         d = np.sqrt((xt - event.x)**2 + (yt - event.y)**2)
-#         ind = d.argmin()
-#         return ind if d[ind] < self.epsilon else None
-
-#     def on_draw(self, event):
-#         """Callback for draws."""
-#         self.background = self.canvas.copy_from_bbox(self.ax.bbox)
-#         self.ax.draw_artist(self.pathpatch)
-#         self.ax.draw_artist(self.line)
-
-#     def on_button_press(self, event):
-#         """Callback for mouse button presses."""
-#         if (event.inaxes is None
-#                 or event.button != MouseButton.LEFT
-#                 or not self.showverts):
-#             return
-#         self._ind = self.get_ind_under_point(event)
-
-#     def on_button_release(self, event):
-#         """Callback for mouse button releases."""
-#         if (event.button != MouseButton.LEFT
-#                 or not self.showverts):
-#             return
-#         self._ind = None
-
-#     def on_key_press(self, event):
-#         """Callback for key presses."""
-#         if not event.inaxes:
-#             return
-#         if event.key == 't':
-#             self.showverts = not self.showverts
-#             self.line.set_visible(self.showverts)
-#             if not self.showverts:
-#                 self._ind = None
-#         self.canvas.draw()
-
-#     def on_mouse_move(self, event):
-#         """Callback for mouse movements."""
-#         if (self._ind is None
-#                 or event.inaxes is None
-#                 or event.button != MouseButton.LEFT
-#                 or not self.showverts):
-#             return
-
-#         vertices = self.pathpatch.get_path().vertices
-
-#         vertices[self._ind] = event.xdata, event.ydata
-#         self.line.set_data(zip(*vertices))
-
-#         self.canvas.restore_region(self.background)
-#         self.ax.draw_artist(self.pathpatch)
-#         self.ax.draw_artist(self.line)
-#         self.canvas.blit(self.ax.bbox)
-
-
-# interactor = PathInteractor(patch)
-# ax.set_title('drag vertices to update path')
-# ax.set_xlim(-3, 4)
-# ax.set_ylim(-3, 4)
-
-# plt.show()
